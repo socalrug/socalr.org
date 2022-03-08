12964114b0c02f377df5f1e07ccf28a9cd56bc67
Javier Orraca <orraca.javier@gmail.com>
2022-03-07 23:29:08 -0800

    Sitewide updates

---
+++ _site/R/packages.R
@@ -0,0 +1,21 @@
+# tidyverse
+library(dplyr)
+library(purrr)
+library(lubridate)
+library(stringr)
+library(readr)
+
+# group info and credentials
+library(googledrive)
+library(meetupr)
+
+# map group locations
+library(leaflet)
+library(ggmap)
+
+# render html
+library(htmltools)
+library(fontawesome)
+
+# avoid base conflict with `filter()`
+conflicted::conflict_prefer("filter", "dplyr")
